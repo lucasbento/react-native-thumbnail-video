1193b7d2748090102a0dcfbb40e87bb1f77f81df
David Narbutovich <vamnemne@gmail.com>
2018-10-06 21:50:54 +0300

    Update getVideoId function

--- src/helpers/youtube.js
+++ src/helpers/youtube.js
@@ -1,5 +1,12 @@
 export const getVideoId = url => {
-  url = url.split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+  const result = url.split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+  const videoIdWithParams = result[2];
 
-  return url[2] !== undefined ? url[2].split(/[^0-9a-z_-]/i)[0] : null;
+  if (videoIdWithParams !== undefined) {
+    const cleanVideoId = videoIdWithParams.split(/[^0-9a-z_-]/i)[0];
+
+    return cleanVideoId;
+  }
+
+  return null;
 };
